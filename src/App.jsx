8867fdd43d9d33f7478568e8ccc412fd1b3ab79d
kamalrajms <kamalrajindtn@gmail.com>
2025-09-04 11:29:29 +0530

    Usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import React, { useContext, createContext } from "react";
-import First from "./component/First";
-
-export const Pass = createContext();
+import React, { useState, useReducer } from "react";
 
 export default function App() {
-  const num = "number";
+  // const [count, setcount] = useState(0);
+
+  const intialvalue = { count: 0 };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "inc":
+        return { count: state.count + 1 };
+      case "dec":
+        return { count: state.count - 1 };
+      case "reset":
+        return { count: 0 };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, intialvalue);
   return (
     <div>
-      <Pass.Provider value={num}>
-        <First />
-      </Pass.Provider>
+      {/* <h2>count :{count}</h2>
+      <button onClick={() => setcount(count + 1)}>inc</button>
+      <br />
+      <button onClick={() => setcount(count - 1)}>dec</button>
+      <br />
+      <button onClick={() => setcount(0)}>rest</button> */}
+      <h2>count :{state.count}</h2>
+      <button onClick={() => dispatch({ type: "inc" })}>inc</button>
+      <br />
+      <button onClick={() => dispatch({ type: "dec" })}>dec</button>
+      <br />
+      <button onClick={() => dispatch({ type: "reset" })}>rest</button>
     </div>
   );
 }
--- src/component/Third.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from "react";
-import { Pass } from "../App";
-
-export default function Third() {
-  const Passname = useContext(Pass);
-  return <div>Third component --{Passname}</div>;
-}
